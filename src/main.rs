5bd687f960bbbe39cd07ac782a643d4996dbda18
koey2182 <koey2182@gmail.com>
2026-02-10 21:11:09 +0900

    fix: jwt exp 필드 추가

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-mod lib;
-
 use axum::{Router, response::{Html, Json}, routing::{get, post}};
+use axum_server::token::{AccessClaims, AccessInfo, AuthError, RefreshClaims, RefreshInfo, generate_access_token, generate_refresh_token};
 use serde::Deserialize;
 use serde_json::{Value, json};
-
-use crate::lib::token::{AuthError, Token, generate_token_set};
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
-    let app = Router::new().route("/", get(handler)).route("/authorize", post(authorize)).route("/protected", get(protected));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/authorize", post(authorize))
+        .route("/protected", get(protected))
+        .route("/refresh", post(refresh));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
@@ -35,16 +36,20 @@ async fn authorize(Json(payload): Json<AuthPayload>) -> Result<Json<Value>, Auth
         return Err(AuthError::WrongCredentials);
     }
 
-    let token = Token {
-        sub: "b@b.com".to_owned(),
-    };
-    let (access_token, refresh_token) = generate_token_set(&token)?;
+    let access_claims = AccessClaims::new("foo".to_owned());
+    let access_token = generate_access_token(access_claims)?;
+    let refresh_claims = RefreshClaims::new("foo".to_owned());
+    let refresh_token = generate_refresh_token(refresh_claims)?;
 
     Ok(Json(json!({"access_token": access_token, "refresh_token": refresh_token})))
 }
 
-async fn protected(token: Token) -> Result<String, AuthError> {
-    Ok(format!("Welcome to the protected area :)\nYour data: \n{token}"))
+async fn protected(access_info: AccessInfo) -> Result<String, AuthError> {
+    Ok(format!("Welcome to the protected area :)\nYour data: {}\n", access_info.user_id))
 } 
 
+async fn refresh(refresh_info: RefreshInfo) -> Result<String, AuthError> {
+    let claims = AccessClaims::new(refresh_info.user_id);
+    generate_access_token(claims)
+}
 
---
+++ src/token.rs
@@ -0,0 +1,137 @@
+use std::sync::LazyLock;
+
+use axum::{Json, RequestPartsExt, extract::FromRequestParts, http::StatusCode, response::IntoResponse};
+use axum_extra::{TypedHeader, headers::{Authorization, authorization::Bearer}};
+use chrono::{Duration, Utc};
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation, decode, encode};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+
+static KEYS: LazyLock<Keys> = LazyLock::new(|| {
+    let access_secret = std::env::var("ACCESS_SECRET").expect("ACCESS_SECRET must be set");
+    let access_secret = access_secret.as_bytes();
+    let refresh_secret = std::env::var("REFRESH_SECRET").expect("REFRESH_SECRET must be set");
+    let refresh_secret = refresh_secret.as_bytes();
+    Keys {
+        access_encoding: EncodingKey::from_secret(access_secret),
+        access_decoding: DecodingKey::from_secret(access_secret),
+        refresh_encoding: EncodingKey::from_secret(refresh_secret),
+        refresh_decoding: DecodingKey::from_secret(refresh_secret),
+    }
+});
+
+static TOKEN_LIFE: LazyLock<TokenLife> = LazyLock::new(|| {
+    let access_minutes = std::env::var("ACCESS_MINUTES").expect("ACCESS_MINUTES must be set").parse::<i64>().expect("ACCESS_MINUTES is not integer");
+    let refresh_days = std::env::var("REFRESH_DAYS").expect("REFRESH_DAYS must be set").parse::<i64>().expect("REFRESH_DAYS is not integer");
+    TokenLife { access_minutes, refresh_days }
+});
+
+struct TokenLife {
+    access_minutes: i64,
+    refresh_days: i64
+}
+
+struct Keys {
+    access_encoding: EncodingKey,
+    access_decoding: DecodingKey,
+    refresh_encoding: EncodingKey,
+    refresh_decoding: DecodingKey,
+}
+
+pub fn generate_access_token(claims: AccessClaims) -> Result<String, AuthError> {
+    let access_token = encode(&Header::default(), &claims, &KEYS.access_encoding).map_err(|_| AuthError::TokenCreation)?;
+    Ok(access_token)
+}
+
+pub fn generate_refresh_token(claims: RefreshClaims)-> Result<String, AuthError> {
+    let refresh_token = encode(&Header::default(), &claims, &KEYS.refresh_encoding).map_err(|_| AuthError::TokenCreation)?;
+    Ok(refresh_token)
+}
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AccessClaims {
+    user_id: String,
+    exp: usize
+}
+impl AccessClaims {
+    pub fn new(user_id: String) -> Self {
+        let exp = Utc::now() + Duration::minutes(TOKEN_LIFE.access_minutes);
+        Self {
+            user_id,
+            exp: usize::try_from(exp.timestamp()).expect("exp is not usize")
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RefreshClaims {
+    user_id: String,
+    exp: usize
+}
+impl RefreshClaims {
+    pub fn new(user_id: String) -> Self {
+        let exp = Utc::now() + Duration::days(TOKEN_LIFE.refresh_days);
+        Self {
+            user_id,
+            exp: usize::try_from(exp.timestamp()).expect("exp is not usize")
+        }
+    }
+}
+
+pub struct AccessInfo {
+    pub user_id: String
+}
+impl<S> FromRequestParts<S> for AccessInfo where S: Send + Sync {
+    type Rejection = AuthError;
+
+    async fn from_request_parts(
+            parts: &mut axum::http::request::Parts,
+            _state: &S,
+        ) -> Result<Self, Self::Rejection> {
+        let TypedHeader(Authorization(bearer)) = parts.extract::<TypedHeader<Authorization<Bearer>>>().await.map_err(|_| AuthError::InvalidToken)?;
+        let token_data = decode::<AccessClaims>(bearer.token(), &KEYS.access_decoding, &Validation::default()).map_err(|err| {println!("{err}"); AuthError::InvalidToken})?;
+        let access_claims = token_data.claims;
+        
+        Ok(AccessInfo { user_id: access_claims.user_id })
+    }
+}
+
+pub struct RefreshInfo {
+    pub user_id: String
+}
+impl <S> FromRequestParts<S> for RefreshInfo where  S: Send + Sync {
+    type Rejection = AuthError;
+
+    async fn from_request_parts(
+            parts: &mut axum::http::request::Parts,
+            _state: &S,
+        ) -> Result<Self, Self::Rejection> {
+        let TypedHeader(Authorization(bearer)) = parts.extract::<TypedHeader<Authorization<Bearer>>>().await.map_err(|_| AuthError::InvalidToken)?;
+        let token_data = decode::<RefreshClaims>(bearer.token(), &KEYS.refresh_decoding, &Validation::default()).map_err(|_| AuthError::InvalidToken)?;
+        Ok(RefreshInfo { user_id: token_data.claims.user_id })
+    }
+}
+
+#[derive(Debug)]
+pub enum AuthError {
+    WrongCredentials,
+    MissingCredentials,
+    TokenCreation,
+    InvalidToken
+}
+impl IntoResponse for AuthError {
+    fn into_response(self) -> axum::response::Response {
+        let (status, error_message) = match self {
+            AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing credentials"),
+            AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, "Toekn creation error"),
+            AuthError::WrongCredentials => (StatusCode::BAD_REQUEST, "Invalid token"),
+            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token")
+        };
+        let body = Json(json!({
+            "error": error_message,
+        }));
+        (status, body).into_response()
+    }
+}
